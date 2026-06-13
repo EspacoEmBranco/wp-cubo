94d2df6c1627d7198507a1e716f53d97542c08fd
João Dias <joao.dias93@gmail.com>
2026-06-13 03:15:47 +0100

    Set up PHPStan static analysis with WordPress stubs

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+// Plugin constants normally defined in entry files at runtime.
+// Pre-declared here so PHPStan can resolve them during static analysis.
+define('CUBO_CORE_VERSION', '1.0.0');
+define('CUBO_CORE_URL', 'http://localhost/');
+define('CUBO_CORE_PATH', __DIR__ . '/docroot/wp-content/plugins/cubo-core/');
+
+define('CUBO_WORK_VERSION', '1.0.0');
+define('CUBO_WORK_URL', 'http://localhost/');
+define('CUBO_WORK_PATH', __DIR__ . '/docroot/wp-content/plugins/cubo-work/');
